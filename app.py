24e4367017e374a7bf9b3198cabe085971c58c15
lorelei888 <melody288293@gmail.com>
2026-04-28 16:07:34 +0800

    Update app.py

--- app.py
+++ app.py
@@ -48,7 +48,7 @@ else:
 def ask():
     if request.method == 'POST':
     # 2. 讀取學生的問題###^#@#Q%#@
-    question = request.form.get('question', '').strip()
+        question = request.form.get('question', '').strip()
         # 3. 查詢題庫的對應答案
         answer = zh_ko_dict.get(question, "抱歉，我目前沒有這個詞的韓文對應。")
         # 4. 回傳答案給學生
