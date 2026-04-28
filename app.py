4097fac260ae8d7a9330e5b2e503b35065f734ec
lorelei888 <melody288293@gmail.com>
2026-04-28 15:35:57 +0800

    Implement stock price input and API request
    Added stock price retrieval functionality using an API.

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-
+import requests #放在最前面? 函數stock內?
 from flask import Flask, request, render_template
 
 app = Flask(__name__)
@@ -26,7 +26,26 @@ zh_ko_dict = {
 def index():
     return render_template('index.html')
 
+# 使用者輸入
+stock_no = input("請輸入股票代號（例如 2330）：")
 
+
+# API URL
+url = f"https://www.twse.com.tw/exchangeReport/STOCK_DAY?response=json&stockNo={stock_no}"
+
+
+
+# 發送請求
+res = requests.get(url) #原始
+data = res.json() #解析後的資料
+
+
+
+# 判斷是否成功
+if data["stat"] == "OK":
+    print("前一天收盤價：",data["data"][-1][6]) #return render_template('stock.html', question=question, answer=answer)
+else:
+    print("查無資料，請確認股票代號或日期") #return render_template('stock.html', question=question, answer=answer)
 @app.route('/ask', methods=['GET', 'POST'])
 def ask():
     if request.method == 'POST':
